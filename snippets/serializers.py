d5b97b00f69121f6e4192ee19f1592d8459d48ba
blue <bluedai180@gmail.com>
2018-03-01 17:01:34 +0800

    Relationships & Hyperlinked APIs

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,23 +3,42 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'snippets')
-
-
-class SnippetSerializer(serializers.ModelSerializer):
-    # ModelSerializer和Django中ModelForm功能相似
-    # Serializer和Django中Form功能相似
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
 
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'snippets')
+
+# ==============================================================================================
+# class UserSerializer(serializers.ModelSerializer):
+#     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+#
+#     class Meta:
+#         model = User
+#         fields = ('id', 'username', 'snippets')
+#
+#
+# class SnippetSerializer(serializers.ModelSerializer):
+#     # ModelSerializer和Django中ModelForm功能相似
+#     # Serializer和Django中Form功能相似
+#     owner = serializers.ReadOnlyField(source='owner.username')
+#
+#     class Meta:
+#         model = Snippet
+#         fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+
+# ===============================================================================================
 # class SnippetSerializer(serializers.Serializer):
 #     # 每一个表都可以建一个serializer，类似Django的Form  专门用于json
 #     id = serializers.IntegerField(read_only=True)
